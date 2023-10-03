d1d6a15f670bddb1e1702bd38c77a728031d9b89
Dima Verzakov <dimaverzakov@Dimas-MacBook-Pro.local>
2023-10-03 17:16:02 +0500

    add screenshot testin to si

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { type ComponentMeta, type ComponentStory } from '@storybook/react'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { Theme } from 'app/providers/ThemeProvider'
