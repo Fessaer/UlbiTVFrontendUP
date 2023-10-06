e28fb6c31d222c402be724f32d9e92c46c72a2fd
Dima Verzakov <dimaverzakov@Dimas-MacBook-Pro.local>
2023-10-06 11:23:32 +0500

    list articles

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,6 +51,7 @@ module.exports = {
     "no-param-reassign": "off",
     "max-len": ["error", { code: 140 }],
     "no-undef": "off",
+    "react/no-array-index-key": "off"
   },
   globals: {
     __IS_DEV__: true,
